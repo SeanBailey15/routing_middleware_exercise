cbfabf512ed66d7dab746330af00ca01431eeac2
Sean Bailey <seanbailey15@gmail.com>
2024-01-29 20:41:05 -0500

    Set up the exercise files

---
+++ app_modules/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const ExpressError = require("./expressError");
+const middleware = require("./middleware");
+const itemsRoutes = require();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/items", itemsRoutes);
+
+app.use((req, res, next) => {
+  const err = new ExpressError("Not Found", 404);
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status,
+    },
+  });
+});
+
+module.exports = app;
---
+++ app_modules/itemsRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const ExpressError = require("./expressError");
+// let items = require("./fakeDb");
+
+const router = new express.Router();
